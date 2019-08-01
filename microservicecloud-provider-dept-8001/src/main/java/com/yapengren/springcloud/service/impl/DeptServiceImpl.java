d73a100075b83b1e90c5629723e76e12645f5bea
yapengren <404210666@qq.com>
2019-08-01 15:16:51 +0800

    REST 微服务架构案例工程模块

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/yapengren/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,31 @@
+package com.yapengren.springcloud.service.impl;
+
+import com.yapengren.springcloud.dao.DeptDao;
+import com.yapengren.springcloud.entities.Dept;
+import com.yapengren.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptDao deptDao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return deptDao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return deptDao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return deptDao.findAll();
+    }
+}
